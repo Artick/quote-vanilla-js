536bcac4b7c6dc1497b0c81e5e65a87a699ccda5
Jorge Anaya <jjam1190@gmail.com>
2021-03-08 20:52:35 -0600

    Fix Target links

--- src/templates/Quote.js
+++ src/templates/Quote.js
@@ -7,10 +7,10 @@ const Quote = async () => {
     <p class="card__quote"> ${data.text} </p>
     <p class="card__quote-author">- ${data.author} -</p>
     <footer>
-    <a href="https://twitter.com/artick">
+    <a href="https://twitter.com/artick" target="_blank">
       <img src="../src/assets/images/twitter.png" />
     </a>
-    <a href="https://github.com/artick">
+    <a href="https://github.com/artick" target="_blank">
       <img src="../src/assets/images/github.png" />
     </a>
     </footer>
