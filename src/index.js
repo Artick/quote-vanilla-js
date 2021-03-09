262d31855d3865977eb0098b77fe14bd8b353a15
Jorge Anaya <jjam1190@gmail.com>
2021-03-08 22:46:02 -0600

    Added webpackloaders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Quote from './templates/Quote.js';
+import './styles/style.css';
 
 (async function App () {
   const main = null || document.getElementById ('main');
-  main.innerHTML = await Quote();
+  main.innerHTML = await Quote ();
 }) ();
